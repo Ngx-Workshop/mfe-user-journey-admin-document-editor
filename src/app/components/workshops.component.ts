e1b02a042932fe5a83e68ac43df2799235643f0c
Wesley DuSell <dusellfam@gmail.com>
2026-01-19 11:57:34 -0500

    Switch out header for shared library

--- src/app/components/workshops.component.ts
+++ src/app/components/workshops.component.ts
@@ -1,24 +1,45 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgxParticleHeader } from '@tmdjr/ngx-shared-headers';
 import { combineLatest, map } from 'rxjs';
 import { NavigationService } from '../services/navigation.service';
-import {
-  HeaderComponent,
-  SidenavHeaderData,
-} from './header.component';
 
 @Component({
   selector: 'ngx-workshops',
-  imports: [RouterModule, HeaderComponent, AsyncPipe],
+  imports: [RouterModule, AsyncPipe, NgxParticleHeader],
   template: `
     @if(viewModel$ | async; as vm) {
-    <ngx-menu-management-header
-      [sidenavHeaderData]="vm.sidenavHeaderData"
-    />
+    <ngx-particle-header>
+      <img [src]="vm.headerSvgPath" />
+      <h1>
+        {{ vm.sectionTitle }}:
+        {{ vm.currentWorkshopTitle ?? 'Workshops' }}
+      </h1>
+    </ngx-particle-header>
     }
     <router-outlet />
   `,
+  styles: [
+    `
+      :host {
+        h1 {
+          font-size: 1.85rem;
+          font-weight: 100;
+          margin: 1.7rem 0;
+        }
+        img {
+          width: 64px;
+          z-index: 2;
+          margin-left: 1.5rem;
+          @media (max-width: 959px) {
+            width: 35px;
+            margin: 0;
+          }
+        }
+      }
+    `,
+  ],
 })
 export class WorkshopsComponent {
   navigationService = inject(NavigationService);
@@ -34,11 +55,9 @@ export class WorkshopsComponent {
         currentWorkshopTitle,
         { headerSvgPath, sectionTitle } = {},
       ]) => ({
-        sidenavHeaderData: {
-          headerSvgPath,
-          sectionTitle,
-          currentWorkshopTitle,
-        } as SidenavHeaderData,
+        headerSvgPath,
+        sectionTitle,
+        currentWorkshopTitle,
       })
     )
   );
